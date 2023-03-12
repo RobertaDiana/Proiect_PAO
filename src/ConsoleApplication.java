fcee8a81e6cf1c330d8e55701dd0aee8547666dd
Alin <alin_popescu02@yahoo.com>
2023-03-12 17:24:37 +0200

    Adaugare ConsoleApplication2 (meniu)

--- src/ConsoleApplication.java
+++ src/ConsoleApplication.java
@@ -1,3 +1,4 @@
+import controler.Controler;
 import models.Persoana;
 
 import java.util.Scanner;
@@ -5,9 +6,10 @@ import java.util.Scanner;
 public class ConsoleApplication {
     public static void main(String[] args) {
 
-        Persoana user=null;
+
+        Controler.init();
         while(true) {
-            conectare(user);
+
             meniu();
 
             Scanner sc = new Scanner(System.in);
@@ -16,51 +18,114 @@ public class ConsoleApplication {
 
             switch (comand) {
                 case "a":
-                    System.out.println("dgwuwbjfbewfbifbrfbrbferufbrufberfberufebubrebrbrbfrbrbfrfrbfefe");
+                    Controler.adaugaCarte();
+                    System.out.println(Controler.carti);
+                    break;
+                case "b":
+                    Controler.adaugaAdresa();
+                    System.out.println(Controler.adrese);
+                    break;
+                case "c":
+                    Controler.adaugaAbonament();
+                    System.out.println(Controler.abonamente);
+                    break;
+                case "d":
+                    Controler.adaugaCategorie();
+                    System.out.println(Controler.categorii);
+                    break;
+                case "e":
+                    Controler.adaugaEditura();
+                    System.out.println(Controler.edituri);
+                    break;
+                case "f":
+                    Controler.adaugaAutor();
+                    System.out.println(Controler.autori);
+                    break;
+                case "g":
+                    Controler.adaugaCititor();
+                    System.out.println(Controler.cititori);
+                case "h":
+
+                    break;
+                case "i":
+                    Controler.afisareAdresa();
+                    System.out.println(Controler.adrese);
+                    break;
+                case "j":
+
+                    break;
+                case "k":
+                    Controler.afisareEditura();
+                    System.out.println(Controler.edituri);
+                    break;
+                case "l":
+
                     break;
                 case "m":
+
+                    break;
+                case "n":
+                    Controler.afisareCategorie();
+                    System.out.println(Controler.categorii);
+                    break;
+                case "o":
+
+                    break;
+                case "p":
+
+                    break;
+                case "q":
+
+                    break;
+                case "r":
+
+                    break;
+                case "s":
+
+                    break;
+                case "t":
                     return;
+                default:
+                    System.out.println("nu ati introdus o comanda valida");
+                    break;
             }
         }
 
     }
 
     private static void meniu(){
-        System.out.println("*********************************");
+
+        System.out.println("************Meniu**************");
 
 
-        System.out.println("************My book**************");
-        System.out.println("*********************************");
 
+        System.out.println("a. Adauga carte");
+        System.out.println("b. Adauga adresa");
+        System.out.println("c. Adauga abonament");
+        System.out.println("d. Adauga categorie");
+        System.out.println("e. Adauga editura");
+        System.out.println("f. Adauga autor");
+        System.out.println("g. Adauga cititor");
 
-        System.out.println("");
+        System.out.println("h. Afisare carti");
+        System.out.println("i. Afisare adresa");
+        System.out.println("j. Afisare abonament");
+        System.out.println("k. Afisare editura");
+        System.out.println("l. Afisare autor");
+        System.out.println("m. Afisare cititor");
+        System.out.println("n. Afisare categorie");
 
+        System.out.println("o. Returneaza carte");
+        System.out.println("p. Imprumuta carte");
 
-        System.out.println("a. Login");
-        System.out.println("b. Logout");
-        System.out.println("c. Creare cont");
-        System.out.println("d. Returneaza carete");
-        System.out.println("e. Imprumuta carte");
-        System.out.println("f. Adauga caret");
-        System.out.println("g. Adauga categorie in lista de categorii");
-        System.out.println("h. Modifica cont");
-        System.out.println("i. Modifica carte");
-        System.out.println("j. Modifica categorie");
-        System.out.println("k. ");
-        System.out.println("l. ");
-        System.out.println("m. Exit");
+        System.out.println("q. Modifica carte");
+        System.out.println("r. Modifica categorie");
+        System.out.println("s. Anuleaza abonament");
+        System.out.println("t. Exit");
 
         System.out.println("Introduceti o comanda:");
 
     }
 
-    private static void conectare(Persoana user){
-        System.out.println("*********************************");
-        if(user!=null){
-            System.out.println("Utilizator : " + user.getNume() + " " + user.getPrenume());
-        }else{
-            System.out.println("Utilizator : neconectat" );
-        }
 
-    }
 }
--- src/controler/Controler.java
+++ src/controler/Controler.java
@@ -2,60 +2,346 @@ package controler;
 
 import models.*;
 
+
+import java.sql.SQLOutput;
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Controler {
-
-    public void test(){
-        Autor autor=new Autor(1, "Ion", "Andrei", null, "F",15);
-        // System.out.println(autor);
-
-        Editura editura= new Editura(1, "Aramis");
-        // System.out.println(editura);
-
-        List<Autor> Autori=new ArrayList<Autor>();
-        Autori.add(autor);
+    public static List<Carte> carti= new ArrayList<>();
+    public static List<Cititor> cititori =new ArrayList<>();
+    public static List<Autor> autori =new ArrayList<>();
+    public static List<Categorie> categorii =new ArrayList<>();
+    public static List<Editura> edituri =new ArrayList<>();
+    public static List<Abonament> abonamente = new ArrayList<>();
+    public static List<Adresa> adrese = new ArrayList<>();
 
 
-        Categorie categorie= new Categorie(1, "Romane", 10);
-        // System.out.println(categorie);
 
-        List<Categorie> categories = new ArrayList<Categorie>();
-        categories.add(categorie);
+    public static void init(){
+        abonamente.add(new Abonament(1,"tip 1",LocalDate.now(),"acctiv"));
+        abonamente.add(new Abonament(2,"tip 2",LocalDate.now(),"acctiv"));
+        abonamente.add(new Abonament(1,"tip 3",LocalDate.now(),"acctiv"));
 
-        Carte carte=new Carte(1, "Verity" ,Autori,categories, LocalDate.now().getYear(),editura,false );
-        // System.out.println(carte);
+        adrese.add(new Adresa());
+        adrese.add(new Adresa(1,"kdnwefnwfef eromfe cednencenec"));
 
-
-        Abonament abonament=new Abonament(1, "lunar", null, "valabil");
-        // System.out.println(abonament);
-
-
-        Adresa adresa = new Adresa(1,"test","7B","nu am detalii");
-        // System.out.println(adresa);
-
-        List<Carte> carti = new ArrayList<Carte>();
-        carti.add(carte);
-
-        Cititor cititor=new Cititor(1, "Ion", "Andrei", null, "F",12,adresa,carti,abonament);
-        // System.out.println(cititor);
-
-//        Cititor c1=new Cititor();
-//        c1.ImprumutaCarte(carte);
-        // System.out.println(c1.ReturneazaCarte(carte));
-        // System.out.println(c1.getCarti());
-
-        // Persoana persoana=new Persoana(1, "Ion", "Andrei", null, "F");
-        // System.out.println(persoana);
-
-        Biblioteca B=new Biblioteca();
-
-        B.adaugaAutor(autor);
-        B.AdaugaCarte(carte);
-        B.AdaugaCarte(new Carte(1, "Atest2" ,Autori,categories, LocalDate.now().getYear(),editura,false));
-        B.SortCarti();
-        System.out.println(B.getCarti());
     }
+
+    public static void adaugaCategorie(){
+
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Id-ul categoriei este: ");
+        int idCategorie;
+
+        while(true)
+        {
+            try {
+                idCategorie = Integer.parseInt(reader.nextLine());
+                break;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Trebuie un numar intreg! Incearca din nou ");
+                System.out.println("Id categorie ");
+            }
+        }
+
+        System.out.println("Numele categoriei este: ");
+        String categorieNume= reader.nextLine().toUpperCase();
+
+        categorii.add(new Categorie(idCategorie,categorieNume));
+    }
+
+    public static void afisareCategorie(){
+        for ( Categorie categorie:categorii) {
+            System.out.println(categorie);
+            System.out.println();
+        }
+    }
+
+    public static void adaugaCititor(){
+
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Id-ul cititorului este: ");
+        int idCititor;
+        while(true)
+        {
+            try {
+                idCititor = Integer.parseInt(reader.nextLine());
+                break;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Trebuie un numar intreg! Incearca din nou ");
+                System.out.println("Id cititor ");
+            }
+        }
+
+        System.out.println("Numele cititorului este: ");
+        String cititorNume= reader.nextLine().toUpperCase();
+
+        System.out.println("Prenumele cititorului este: ");
+        String cititorPrenume=reader.nextLine().toUpperCase();
+
+        System.out.println("Data nasterii este: ");
+        LocalDate cititorDataNasterii;
+        while(true)
+        {
+            try {
+                cititorDataNasterii=LocalDate.parse(reader.nextLine());
+                break;
+            }
+            catch(DateTimeParseException e)
+            {
+                System.out.println("Trebuie o data valida! Incearca din nou ");
+                System.out.println("Data nasterii este: ");
+            }
+        }
+
+        System.out.println("Gen:  ");
+        String gen = reader.nextLine().toUpperCase();
+
+        System.out.println("Nr. carti citite: ");
+        int nrCartiCitite;
+        while(true)
+        {
+            try {
+                nrCartiCitite = Integer.parseInt(reader.nextLine());
+                break;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Trebuie un numar intreg! Incearca din nou ");
+                System.out.println("Nr. carti citite: ");
+            }
+        }
+
+        System.out.println("Adresa , veti introduce id-ul adresei: ");
+        afisareAdresa();
+
+        System.out.println("Id Adresa ");
+        int idAdresa;
+        while(true)
+        {
+            try {
+                idAdresa = Integer.parseInt(reader.nextLine());
+                break;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Trebuie un numar intreg! Incearca din nou ");
+                System.out.println("Id Adresa: ");
+            }
+        }
+        Adresa adresa=null;
+        for (Adresa a:adrese) {
+            if(a.getIdAdresa()==idAdresa){
+                adresa=a;
+                break;
+            }
+        }
+
+
+        System.out.println("Abonamnet , veti introduce id-ul abonamentului: ");
+        afisareAdresa();
+
+        System.out.println("Id Abonamnet ");
+        int idAbonamnet;
+        while(true)
+        {
+            try {
+                idAbonamnet = Integer.parseInt(reader.nextLine());
+                break;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Trebuie un numar intreg! Incearca din nou ");
+                System.out.println("Id Abonamnet: ");
+            }
+        }
+        Abonament abonament=null;
+        for (Abonament a:abonamente) {
+            if(a.getIdAbonament()==idAbonamnet){
+                abonament=a;
+                break;
+            }
+        }
+
+
+
+        cititori.add(new Cititor(idCititor,cititorNume,cititorPrenume,cititorDataNasterii,gen,nrCartiCitite,adresa,new ArrayList<Carte>(),abonament));
+    }
+
+    public static void adaugaAutor(){
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Id-ul cititorului este: ");
+        int id;
+        while(true)
+        {
+            try {
+                id = Integer.parseInt(reader.nextLine());
+                break;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Trebuie un numar intreg! Incearca din nou ");
+                System.out.println("Id cititor ");
+            }
+        }
+
+        System.out.println("Numele cititorului este: ");
+        String Nume= reader.nextLine().toUpperCase();
+
+        System.out.println("Prenumele cititorului este: ");
+        String Prenume=reader.nextLine().toUpperCase();
+
+        System.out.println("Data nasterii este: ");
+        LocalDate DataNasterii;
+        while(true)
+        {
+            try {
+                DataNasterii=LocalDate.parse(reader.nextLine());
+                break;
+            }
+            catch(DateTimeParseException e)
+            {
+                System.out.println("Trebuie o data valida! Incearca din nou ");
+                System.out.println("Data nasterii este: ");
+            }
+        }
+
+        System.out.println("Gen:  ");
+        String gen = reader.nextLine().toUpperCase();
+
+        System.out.println("Nr. carti scrise: ");
+        int nrCartiScrise;
+        while(true)
+        {
+            try {
+                nrCartiScrise = Integer.parseInt(reader.nextLine());
+                break;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Trebuie un numar intreg! Incearca din nou ");
+                System.out.println("Nr. carti scrise: ");
+            }
+        }
+
+        autori.add(new Autor(id,Nume,Prenume,DataNasterii,gen,nrCartiScrise));
+    }
+
+    public static void adaugaAbonament(){
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Id-ul abonamentului este: ");
+        int idAbonament;
+        while(true)
+        {
+            try {
+                idAbonament = Integer.parseInt(reader.nextLine());
+                break;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Trebuie un numar intreg! Incearca din nou ");
+                System.out.println("Id abonament ");
+            }
+        }
+
+        System.out.println("Tipul abonamentului este: ");
+        String tipAbonament= reader.nextLine().toUpperCase();
+    }
+
+    public static void adaugaAdresa(){
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Id-ul locatiei este: ");
+        int idAdresa;
+        while(true)
+        {
+            try {
+                idAdresa = Integer.parseInt(reader.nextLine());
+                break;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Trebuie un numar intreg! Incearca din nou ");
+                System.out.println("Id adresa ");
+            }
+        }
+
+        System.out.println("Locatia este: ");
+        String adresalocatie= reader.nextLine().toUpperCase();
+        adrese.add(new Adresa(idAdresa, adresalocatie));
+
+    }
+
+    public static void afisareAdresa(){
+        for ( Adresa adresa:adrese) {
+            System.out.println(adresa);
+            System.out.println();
+        }
+    }
+
+    public static void adaugaCarte() {
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Id-ul cartii este: ");
+        int idCarte;
+        while(true)
+        {
+            try {
+                idCarte = Integer.parseInt(reader.nextLine());
+                break;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Trebuie un numar intreg! Incearca din nou ");
+                System.out.println("Id carte ");
+            }
+        }
+
+        System.out.println("Numele cartii este: ");
+        String carteNume= reader.nextLine().toUpperCase();
+    }
+
+
+    public static void adaugaEditura(){
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Id-ul editurii este: ");
+        int idEditura;
+        while(true)
+        {
+            try {
+                idEditura = Integer.parseInt(reader.nextLine());
+                break;
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Trebuie un numar intreg! Incearca din nou ");
+                System.out.println("Id editura ");
+            }
+        }
+
+        System.out.println("Numele editurii este: ");
+        String edituraNume= reader.nextLine().toUpperCase();
+
+        edituri.add(new Editura(idEditura, edituraNume));
+    }
+    public static void afisareEditura(){
+        for ( Editura editura:edituri) {
+            System.out.println(editura);
+            System.out.println();
+        }
+    }
+
+    public static void SortCarti(){
+        Collections.sort(carti, new CarteComparator());
+    }
+
 }
